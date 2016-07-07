b9b4c4f709e4abbd1ce14158a1510a20b3a01ed2
Brent Perry <burentoperii@gmail.com>
2016-07-07 09:15:13 -0500

    Initial commit of ConvertToNumber.

--- ConvertToNumber/main.cpp
+++ ConvertToNumber/main.cpp
@@ -6,10 +6,27 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    string input;
+    
+    cout << "Please type out the word for a number (e.g. 'one'): ";
+    cin >> input;
+    
+    if (input == "one")
+        cout << "1\n";
+    else if (input == "zero")
+        cout << "0\n";
+    else if (input == "two")
+        cout << "2\n";
+    else if (input == "three")
+        cout << "3\n";
+    else if (input == "four")
+        cout << "4\n";
+    else
+        cout << "Not a number I know.\n";
+    
     return 0;
 }
